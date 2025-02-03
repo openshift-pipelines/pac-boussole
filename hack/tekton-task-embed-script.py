26566d6a774954ccee2b745bb68733dd61407208
Chmouel Boudjnah <chmouel@redhat.com>
2025-02-03 18:25:00 +0100

    Use only params to the base PR

--- hack/tekton-task-embed-script.py
+++ hack/tekton-task-embed-script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2021 The Tekton Authors
+# Copyright 2025 Chmouel Boudjnah <chmouel@redhat.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Launch with uv run or install the dependency by other mean
+#
 # /// script
 # requires-python = ">=3.12"
 # dependencies = [
